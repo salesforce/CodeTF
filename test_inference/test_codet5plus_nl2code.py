c69c3b430e5ab044df88c8f51a11d57a00babee2
Nghi Bui <nghi.bui@salesforce.com>
2023-06-06 08:51:34 +0000

    update inferencing samples

--- test_inference/test_codet5plus_nl2code.py
+++ test_inference/test_codet5plus_nl2code.py
@@ -3,8 +3,8 @@ from pathlib import Path
 sys.path.append(str(Path(".").absolute().parent))
 from codetf.models import load_model_pipeline
 
-code_generation_model = load_model_pipeline(model_name="causallm", task="pretrained",
-            model_type="codegen-350M-mono", is_eval=True,
+code_generation_model = load_model_pipeline(model_name="codet5", task="pretrained",
+            model_type="plus-770M-python", is_eval=True,
             load_in_8bit=True, load_in_4bit=False, weight_sharding=False)
             
 result = code_generation_model.predict(["def print_hello_world():"])
