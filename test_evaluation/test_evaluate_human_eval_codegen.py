5661a9dcf6c93d593ce332369798f0584cbe5962
Nghi Bui <nghi.bui@salesforce.com>
2023-06-06 08:52:31 +0000

    update evaluation with codegeng

--- test_evaluation/test_evaluate_human_eval_codegen.py
+++ test_evaluation/test_evaluate_human_eval_codegen.py
@@ -10,7 +10,7 @@ import os
 os.environ["HF_ALLOW_CODE_EVAL"] = "1"
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
-model_class = load_model_pipeline(model_name="causal-lm", task="pretrained",
+model_class = load_model_pipeline(model_name="causallm", task="pretrained",
             model_type="codegen-350M-mono", is_eval=True,
             load_in_8bit=True, weight_sharding=False)
 
